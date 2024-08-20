6bb9c1836c48533e0def8e410ef936dc9a01c5fa
MathavK-raptee <mathavk@raptee.com>
2024-08-20 23:58:30 +0530

    Err10

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,5 @@
 # gunicorn.conf.py
-bind = "0.0.0.0"
+bind = "0.0.0.0:$PORT"
 workers = 3
-#user = "myusername"
-#group = "mygroup"
 loglevel = "info"
-#accesslog = "/path/to/access.log"
-#errorlog = "/path/to/error.log"
-#pidfile = "/path/to/gunicorn.pid"
-daemon = True
-
 wsgi_app = "portfolio.wsgi:app"
