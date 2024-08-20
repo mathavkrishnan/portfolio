7ad171887219c0f9c2a70d1243c90148e674c6e2
MathavK-raptee <mathavk@raptee.com>
2024-08-21 00:31:08 +0530

    Err18

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,12 +8,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
+#import sys
 
 from django.core.wsgi import get_wsgi_application
 
-project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(project_path)
+#project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#sys.path.append(project_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
