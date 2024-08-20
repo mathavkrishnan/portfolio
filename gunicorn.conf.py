ec0b50dbed24f85a19f09b957b9dbcfff7708595
MathavK-raptee <mathavk@raptee.com>
2024-08-20 23:33:09 +0530

    Err4

---
+++ gunicorn.conf.py
@@ -0,0 +1,12 @@
+# gunicorn.conf.py
+bind = "0.0.0.0:8000"
+workers = 3
+user = "myusername"
+group = "mygroup"
+loglevel = "debug"
+#accesslog = "/path/to/access.log"
+#errorlog = "/path/to/error.log"
+#pidfile = "/path/to/gunicorn.pid"
+daemon = True
+
+wsgi_app = "portfolio.wsgi:application"
